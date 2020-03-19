a0ae60ddad13bd67b183b6ebb9300e3b1d36e72d
Nicolaj Nielsen <nicolaj.niels1@gmail.com>
2020-03-19 22:56:49 +0100

    starting from checkpoint

--- src/Fib.js
+++ src/Fib.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-class Fib extends Components {
+class Fib extends Component {
   state = {
     seenIndexes: [],
     values: {},
@@ -11,33 +11,33 @@ class Fib extends Components {
   componentDidMount() {
     this.fetchValues();
     this.fetchIndexes();
-
   }
 
   async fetchValues() {
     const values = await axios.get('/api/values/current');
-    this.setState({ values: values.data});
+    this.setState({ values: values.data });
   }
 
   async fetchIndexes() {
-    const seenIndexes = await axios.get('api/values/all');
-    this.setState({seenIndexes: seenIndexes.data});
+    const seenIndexes = await axios.get('/api/values/all');
+    this.setState({
+      seenIndexes: seenIndexes.data
+    });
   }
 
-  handleSubmit = async (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
 
     await axios.post('/api/values', {
       index: this.state.index
     });
-    this.setState( { index: ''});
-  }
-
+    this.setState({ index: '' });
+  };
 
   renderSeenIndexes() {
     return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
-  
+
   renderValues() {
     const entries = [];
 
@@ -55,21 +55,22 @@ class Fib extends Components {
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit} >
+        <form onSubmit={this.handleSubmit}>
           <label>Enter your index:</label>
           <input
             value={this.state.index}
-            onChange={event => this.setState({ index: event.target.value} )} 
+            onChange={event => this.setState({ index: event.target.value })}
           />
           <button>Submit</button>
         </form>
 
         <h3>Indexes I have seen:</h3>
         {this.renderSeenIndexes()}
-        <h3>Calculated values:</h3>
+
+        <h3>Calculated Values:</h3>
         {this.renderValues()}
       </div>
-    )
+    );
   }
 }
 
